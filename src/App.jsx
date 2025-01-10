74b329654767de1dce169e3044847f46ad18a528
Rabyn-Collab <babynshrestha64@gmail.com>
2025-01-10 08:54:39 +0545

    changes

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello jee</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni vitae autem tempore rem nesciunt sint ullam. Harum error explicabo illum culpa possimus unde perspiciatis, distinctio cum voluptas repudiandae magni magnam?</p>
+
+    </div>
+  )
+}
+
+export default App
