6350e247428aa14e372bacc4b5bc6e6774be863e
Rabyn-Collab <babynshrestha64@gmail.com>
2025-01-10 08:35:16 +0545

    changes

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+const a = [11, 44, 5, 6, 7, 8, 55,];
+const b = [11, 44, 5, 6, 7, 8, 55,];
+
+const unique = [];
+
+const m = new Set([11, 22, 11, 33, 44, 55, 77]);
+
+m.forEach((n) => {
+  console.log(n);
+})
