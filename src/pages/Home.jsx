a5c1b07e439eda5443cd9c7032f8baf8b32b7bc1
Rabyn-Collab <babynshrestha64@gmail.com>
2025-01-19 08:28:29 +0545

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from '../components/Header'
+import { NavLink, Outlet, } from 'react-router'
 
 const Home = () => {
   return (
@@ -8,6 +9,12 @@ const Home = () => {
       <Header />
 
       <h1 className='text-2xl font-bold'>This is Home Page</h1>
+
+
+      <NavLink to={'/page1'}>Page1</NavLink>
+      <NavLink to={'/page2'}>Page2</NavLink>
+
+      <Outlet />
 
 
     </div>
