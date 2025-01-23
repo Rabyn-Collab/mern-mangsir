b9cfb421b67f9abf2c1bb5c5a4f52c0283168037
Rabyn-Collab <babynshrestha64@gmail.com>
2025-01-23 08:26:05 +0545

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,8 @@
 import React from 'react'
-import DisplaySection from '../components/DisplaySection'
-import Tech from '../components/Tech'
-import Info from '../components/Info'
 
 const Home = () => {
-
-
   return (
-    <div className='px-10'>
-
-      <DisplaySection />
-      <Tech />
-      <Info />
-
+    <div>
 
     </div>
   )
