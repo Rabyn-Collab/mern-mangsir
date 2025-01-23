a10adafe6ded81a9506ad8aadbf4dd195ac51331
Rabyn-Collab <babynshrestha64@gmail.com>
2025-01-23 08:58:38 +0545

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,35 @@
+import { Button, Typography } from '@material-tailwind/react'
 import React from 'react'
+import { useState } from 'react';
+
+
 
 const Home = () => {
+
+  const some = useState();
+
+  const persons = ['ram', 'shyam', 'hari'];
+
+  console.log(some);
+
+  let count = 0;
+
+  const increment = () => {
+    count++;
+    console.log(count);
+  }
+
   return (
-    <div>
+    <div className='p-5'>
+
+      <Typography variant='h3'>{count}</Typography>
+
+
+
+      <Button
+        onClick={increment}
+        size='sm' color='red'>Click Me</Button>
+
 
     </div>
   )
