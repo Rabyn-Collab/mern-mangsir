d80b8544c94293322c1f49b6d57323be728d2ffe
Rabyn-Collab <babynshrestha64@gmail.com>
2025-03-06 07:58:54 +0545

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    chunkSizeWarningLimit: 1600
+    chunkSizeWarningLimit: 1600,
+    outDir: 'dist',
   },
   plugins: [react()],
 })
