8032ccb29799e512a10dcaf2e1af61397cf21fe3
Rabyn-Collab <babynshrestha64@gmail.com>
2025-03-06 07:32:31 +0545

    changes

--- src/app/apiUrls.js
+++ src/app/apiUrls.js
@@ -1,2 +1,6 @@
-export const base = 'http://192.168.1.97:5000';
-export const baseUrl = 'http://192.168.1.97:5000/api';
+// export const base = 'http://192.168.1.97:5000';
+// export const baseUrl = 'http://192.168.1.97:5000/api';
+
+
+export const base = 'https://mern-mangsir.onrender.com';
+export const baseUrl = 'https://mern-mangsir.onrender.com/api';
