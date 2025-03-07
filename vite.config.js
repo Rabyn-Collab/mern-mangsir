365965405df65aaad47b0c0cd1ffd4e8bb95bf5b
Rabyn-Collab <babynshrestha64@gmail.com>
2025-03-07 07:32:27 +0545

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default {
   build: {
-    chunkSizeWarningLimit: 1600,
     outDir: 'dist',
   },
-  base: './', // Use `./` if you get path errors
-  plugins: [react()],
-});
+}
